90543168ca4e7553dee8321bbec7061398f9640f
RandomMushroom <117697813+mwhroom@users.noreply.github.com>
2026-02-19 11:08:31 -0500

    better comments for example

--- tests/pycairotest.py
+++ tests/pycairotest.py
@@ -52,21 +52,20 @@ class MyWindow:
             self.hinst, # Module handle instance
             None # Extra creation parameters
         )
-        win32gui.SetWindowLong(self.hwnd, win32con.GWL_EXSTYLE, style)
 
-        # Make window always stay on top
+        # this is required for some reason even though we already create the window with the style
+        win32gui.SetWindowLong(self.hwnd, win32con.GWL_EXSTYLE, style) 
         
-        
+        # key white out to be a transparent color
         win32gui.SetLayeredWindowAttributes(self.hwnd, win32api.RGB(255, 255, 255), 0, win32con.LWA_COLORKEY)
 
+        # Make window always stay on top
         win32gui.SetWindowPos(
             self.hwnd,
             win32con.HWND_TOPMOST,  # Place the window at the top of the Z order
             0, 0, 0, 0,             # No change to x, y, width, or height (params are ignored due to flags)
             win32con.SWP_NOMOVE | win32con.SWP_NOSIZE | win32con.SWP_NOACTIVATE | win32con.SWP_FRAMECHANGED
         )
-
-        
 
         # Update and show window
         self.draw_image()
@@ -76,13 +75,7 @@ class MyWindow:
         # Start main thread
         self.main_thread.start()
 
-    # When the X button is clicked, or window is destroyed in any way
-    def OnDestroy(self, hwnd, message, wparam, lparam):
-        print("attempting to close")
-        self.stop_main()
-        win32gui.PostQuitMessage(0)
-        return True
-
+    # Stop main thread
     def stop_main(self):
         print("stopping main")
         self.main_thread.stop()
@@ -93,10 +86,15 @@ class MyWindow:
     # Manage all processes, such as paint calls, input events, etc.
     # Inputs: Window obj, window handle, type of update, wparam for specific msg, lparam for specific msg
     def wnd_proc(self, hwnd, msg, wparam, lparam):
+        # Called every paint frame (a lot)
         if msg == win32con.WM_PAINT:
             self.on_paint()
+        
+        # Idk what the rest of these do, can't seem to make them work
         elif msg == win32con.WM_DESTROY:
-            return self.OnDestroy(hwnd, msg, wparam, lparam)
+            self.stop_main()
+            win32gui.PostQuitMessage(0)
+            return True
         elif msg == win32con.WM_CLOSE:
             win32gui.DestroyWindow(self.hwnd)
             self.stop_main()
@@ -110,6 +108,7 @@ class MyWindow:
     def on_paint(self):
         pass
 
+    # Main loop on separate thread
     def main_loop(self):
         while 1:
             time.sleep(1/30) # 30 fps
@@ -123,6 +122,7 @@ class MyWindow:
             self.draw_image()
         
 
+    # draw the circle
     def draw_image(self):
         # Device context handle (in this case, the window)
         hdc = win32gui.GetDC(self.hwnd)
@@ -130,16 +130,13 @@ class MyWindow:
         # Normal imp: 
         # surface = cairo.ImageSurface(cairo.FORMAT_WIN32_SURFACE, WIDTH, HEIGHT)
         # Win specific imp:
-        surface = cairo.Win32Surface(hdc)
+        surface = cairo.Win32Surface(hdc) # create cairo surface
 
-        ctx = cairo.Context(surface)
-
-        ctx.scale(1, 1)  # Normalizing the canvas
-
+        ctx = cairo.Context(surface) # create cairo context on surface
         ctx.set_antialias(cairo.ANTIALIAS_NONE) # No antialiasing
 
         # create pattern
-        # edited to just fill with white
+        # edited code to just fill with white
         pat = cairo.LinearGradient(0.0, 0.0, 0.0, 1.0)
         pat.add_color_stop_rgba(1, 1, 1, 1, 1)  # First stop, 50% opacity
         pat.add_color_stop_rgba(1, 1, 1, 1, 1)  # Last stop, 0% opacity
@@ -149,28 +146,26 @@ class MyWindow:
         ctx.set_source(pat)
         ctx.fill()
 
+        # make circle pos loop screen
         rad = 20
         if testglobal.position[0]>=screen_width+rad:
             testglobal.position[0] = -rad
         if testglobal.position[1]>=screen_height+rad:
             testglobal.position[1] = -rad
 
-        ctx.translate(testglobal.position[0], testglobal.position[1])  # Changing the current transformation matrix
+        # set relative position
+        ctx.translate(testglobal.position[0], testglobal.position[1])
 
-        # Arc(cx, cy, radius, start_angle, stop_angle)
-        #ctx.arc(0.2, 0.1, 0.1, -math.pi / 2, 0)
-        #ctx.line_to(0.5+time.process_time(), 0.1)  # Line to (x,y)
-        # Curve(x1, y1, x2, y2, x3, y3)
-        #ctx.curve_to(0.5, 0.2, 0.5, 0.4, 0.2, 0.8)
-        #ctx.close_path()
+        # draw circle
         ctx.arc(0.0, 0.0, rad, 0.0, 2.0*math.pi)
         ctx.close_path()
         ctx.set_source_rgba(0.0, 0.0, .7, .5)
         ctx.fill_preserve()
-        ctx.set_source_rgb(0.3, 0.3, 0.3)  # Solid color
+        ctx.set_source_rgb(0.3, 0.3, 0.3)
         ctx.set_line_width(4)
         ctx.stroke()
         
+        # flush surface (must be done at end of every drawing)
         surface.flush()
 
         # Release device context handle to be used somewhere else
@@ -182,8 +177,4 @@ class MyWindow:
 if __name__=="__main__":
     w = MyWindow()
     random.seed(time.time())
-
     win32gui.PumpMessages()
-
-
-
